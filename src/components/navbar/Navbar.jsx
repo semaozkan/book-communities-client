be85767c71e7a5771e9552e1f87fc26d1326ebe5
Semanur Özkan <semanurozkan4444@gmail.com>
2024-12-28 20:44:59 +0300

    feat: create navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from "./navbar.module.scss";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className={styles.container}></div>
+    </div>
+  );
+};
+
+export default Navbar;
