c1cc93238e6c5fc723325ab4caafb6106ca8a6c3
Semanur Özkan <semanurozkan4444@gmail.com>
2025-03-23 17:34:22 +0300

    feat(navbar): create navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,38 @@
 import styles from "./navbar.module.scss";
+import { FaHeart } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className={styles.container}></div>
+    <div className={styles.navbar}>
+      <div className={styles.container}>
+        <div className={styles.logoContainer}>
+          <h1>
+            <a href="/">BİBLİYOFİL</a>
+          </h1>
+        </div>
+        <div className={styles.navCenter}>
+          <li>
+            <a href="">Sesli Kitaplar</a>
+          </li>
+
+          <li>
+            <a href="">Kütüphanem</a>
+          </li>
+
+          <li className={styles.fav}>
+            <FaHeart className={styles.heart} />
+            <a href="">Favorilerim</a>
+          </li>
+        </div>
+        <div className={styles.navEnd}>
+          <button>
+            <a href="/login">Giriş Yap</a>
+          </button>
+          <button>
+            <a href="/register">Kaydol</a>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
