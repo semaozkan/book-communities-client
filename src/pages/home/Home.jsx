45190d9038b89fa9853973a301c3bb63c7e8f681
Semanur Özkan <semanurozkan4444@gmail.com>
2025-03-24 22:21:14 +0300

    feat(home): add first section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,20 @@
+import BookCard from "../../components/bookCard/BookCard";
 import styles from "./home.module.scss";
 
 const Home = () => {
-  return <div className={styles.container}>Home</div>;
+  return (
+    <div className={styles.home}>
+      <div className={styles.firstSection}>
+        <div className={styles.imageContainer}>
+          <img src="/images/home.jpg" alt="" />
+        </div>
+        <div className={styles.searchBar}>
+          <input type="text" placeholder="Kitap ara" />
+          <button>Ara</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
