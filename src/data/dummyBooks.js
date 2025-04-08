711da9f56eb6849815d0d7ac71f513a831873db2
Semanur Özkan <semanurozkan4444@gmail.com>
2025-04-08 21:08:54 +0300

    feat(data): add dummy data

---
+++ src/data/dummyBooks.js
@@ -0,0 +1,156 @@
+// src/data/dummyBooks.js
+
+const dummyBooks = [
+  {
+    id: 1,
+    title: "Küçük Prens",
+    author: "Antoine de Saint-Exupéry",
+    image: "images/kucuk_prens.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 1,
+    title: "Küçük Prens",
+    author: "Antoine de Saint-Exupéry",
+    image: "images/kucuk_prens.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 1,
+    title: "Küçük Prens",
+    author: "Antoine de Saint-Exupéry",
+    image: "images/kucuk_prens.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 1,
+    title: "Küçük Prens",
+    author: "Antoine de Saint-Exupéry",
+    image: "images/kucuk_prens.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 1,
+    title: "Küçük Prens",
+    author: "Antoine de Saint-Exupéry",
+    image: "images/kucuk_prens.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 1,
+    title: "Küçük Prens",
+    author: "Antoine de Saint-Exupéry",
+    image: "images/kucuk_prens.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 1,
+    title: "Küçük Prens",
+    author: "Antoine de Saint-Exupéry",
+    image: "images/kucuk_prens.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 1,
+    title: "Küçük Prens",
+    author: "Antoine de Saint-Exupéry",
+    image: "images/kucuk_prens.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 1,
+    title: "Küçük Prens",
+    author: "Antoine de Saint-Exupéry",
+    image: "images/kucuk_prens.jpg",
+  },
+  {
+    id: 2,
+    title: "Simyacı",
+    author: "Paulo Coelho",
+    image: "/images/simyacı.jpg",
+  },
+  {
+    id: 1,
+    title: "Küçük Prens",
+    author: "Antoine de Saint-Exupéry",
+    image: "images/kucuk_prens.jpg",
+  },
+  {
+    id: 1,
+    title: "Küçük",
+    author: "Antoine de Saint-Exupéry",
+    image: "images/kucuk_prens.jpg",
+  },
+];
+
+export default dummyBooks;
