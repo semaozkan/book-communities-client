378dff9c3545054d2f6e7320693120aa61ed93fe
Semanur Özkan <semanurozkan4444@gmail.com>
2025-04-08 21:12:08 +0300

    feat(home): add second section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
-import BookCard from "../../components/bookCard/BookCard";
+// import BookCard from "../../components/bookCard/BookCard";
+import Slider from "../../components/slider/Slider";
 import styles from "./home.module.scss";
 
 const Home = () => {
@@ -13,6 +14,13 @@ const Home = () => {
           <button>Ara</button>
         </div>
       </div>
+      <div className={styles.secondSection}>
+        <div className={styles.secondSectionTitle}>Sesli Kitaplar</div>
+        <div className={styles.books}>
+          <Slider />
+        </div>
+      </div>
+      <div className={styles.thirdSection}></div>
     </div>
   );
 };
