71c48cb05d6b2cde1ebd688794a4de5b95a0fba3
Semanur Özkan <semanurozkan4444@gmail.com>
2025-04-26 14:27:56 +0300

    feat(app): add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Audiobooks from "./pages/audiobooks/Audiobooks";
+import Communities from "./pages/communities/Communities";
+import BookDetail from "./pages/bookDetail/BookDetail";
+import SingleCommunity from "./pages/singleCommunity/SingleCommunity";
 
 function App() {
   return (
@@ -12,6 +17,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/audiobooks" element={<Audiobooks />} />
+          <Route path="/communities" element={<Communities />} />
+          <Route path="/book/:id" element={<BookDetail />} />
+          <Route path="/community" element={<SingleCommunity />} />
         </Routes>
       </BrowserRouter>
     </>
