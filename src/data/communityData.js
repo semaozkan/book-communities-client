846dc3258c403e9f46e9e47257db40a8b835a463
Semanur Özkan <semanurozkan4444@gmail.com>
2025-05-12 21:46:00 +0300

    feat: create communityData

---
+++ src/data/communityData.js
@@ -0,0 +1,21 @@
+export const communityData = [
+  {
+    id: 1,
+    name: "Paüsiber Kitap Topluluğu",
+    members: 10,
+    image: "/images/pausiber_kitap_topluluğu.png",
+  },
+  {
+    id: 2,
+    name: "Psikoloji Kitap Kulübü",
+    members: 7,
+    image: "/images/psikoloji.png",
+  },
+  {
+    id: 3,
+    name: "Kadın Yazarlar Kulübü",
+    members: 15,
+    image: "/images/kadin_yazarlar.png",
+  },
+  // ...daha fazla topluluk
+];
