71ca00519a65a8027c275c8e1a6ee999e033c88b
Semanur Özkan <semanurozkan4444@gmail.com>
2025-05-12 21:52:14 +0300

    feat(navbar): show buttons if user exist

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,17 @@
 import styles from "./navbar.module.scss";
 import { FaHeart } from "react-icons/fa";
+import { BsThreeDots } from "react-icons/bs";
+import { FaUserCircle } from "react-icons/fa";
+import { useRef, useState } from "react";
+import { MdOutlineAccountBox } from "react-icons/md";
+import { IoIosLogOut } from "react-icons/io";
 
 const Navbar = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const modalRef = useRef(null);
+  const buttonRef = useRef(null);
+  const [user, setUser] = useState(true);
+
   return (
     <div className={styles.navbar}>
       <div className={styles.container}>
@@ -29,12 +39,44 @@ const Navbar = () => {
           </li>
         </div>
         <div className={styles.navEnd}>
-          <button>
-            <a href="/login">Giriş Yap</a>
-          </button>
-          <button>
-            <a href="/register">Kaydol</a>
-          </button>
+          {!user ? (
+            <div>
+              <button>
+                <a href="/login">Giriş Yap</a>
+              </button>
+            </div>
+          ) : (
+            <div
+              className={styles.user}
+              onClick={() => {
+                setIsModalOpen(!isModalOpen);
+              }}
+            >
+              <li>
+                <BsThreeDots className={styles.dots} />
+              </li>
+              <li>
+                <FaUserCircle className={styles.circle} />
+              </li>
+            </div>
+          )}
+
+          {isModalOpen && (
+            <div className={styles.modal} ref={modalRef}>
+              <div className={styles.modalItem} ref={buttonRef}>
+                <MdOutlineAccountBox className={styles.accountIcon} />
+                <a href="/profile">Hesabım</a>
+              </div>
+              <div
+                className={styles.modalItem}
+                ref={buttonRef}
+                // onClick={handleLogout}
+              >
+                <IoIosLogOut className={styles.logoutIcon} />
+                Çıkış Yap
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
