6885e7d153fd040ea8a41ca14bf4d735714e7e94
Semanur Özkan <semanurozkan4444@gmail.com>
2025-05-14 16:53:43 +0300

    feat(auth): add login form logic with validation, error handling, and navigation

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,49 @@
 import { useState } from "react";
 import styles from "./login.module.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const FETCH = import.meta.env.VITE_FETCH_URL;
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   console.log(email, password);
+
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+    const formData = new FormData(e.target);
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await axios.post(`${FETCH}auth/login`, {
+        email,
+        password,
+      });
+
+      localStorage.setItem("user", JSON.stringify(res.data));
+
+      console.log(res);
+
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data.message);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={styles.loginContainer}>
       <div className={styles.imageContainer}>
@@ -17,7 +55,7 @@ const Login = () => {
             <h2>Giriş Yap</h2>
             <p>Sesli kitap dünyasına hoş geldin</p>
           </div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className={styles.formRow}>
               <label htmlFor="email">E-Posta:</label>
               <input
@@ -27,6 +65,7 @@ const Login = () => {
                 placeholder="Lütfen e-posta adresinizi girin"
                 required
                 value={email}
+                onFocus={() => setError("")}
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
@@ -40,6 +79,7 @@ const Login = () => {
                 placeholder="Lütfen parolanızı girin"
                 required
                 value={password}
+                onFocus={() => setError("")}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
@@ -51,6 +91,13 @@ const Login = () => {
                 Kayıt Ol
               </Link>
             </p>
+
+            {error && (
+              <div className={styles.errorBox}>
+                <span className={styles.icon}>❗</span>
+                <span>{error}</span>
+              </div>
+            )}
           </form>
         </div>
       </div>
