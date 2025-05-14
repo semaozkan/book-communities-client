30b0a20cef3932711afc377d9984a09f76e809e3
Semanur Özkan <semanurozkan4444@gmail.com>
2025-05-14 16:36:37 +0300

    fix: update community data

--- src/data/communityData.js
+++ src/data/communityData.js
@@ -9,13 +9,13 @@ export const communityData = [
     id: 2,
     name: "Psikoloji Kitap Kulübü",
     members: 7,
-    image: "/images/psikoloji.png",
+    image: "../../public/images/simyacı.jpg",
   },
   {
     id: 3,
     name: "Kadın Yazarlar Kulübü",
     members: 15,
-    image: "/images/kadin_yazarlar.png",
+    image: "/images/about.png",
   },
   // ...daha fazla topluluk
 ];
