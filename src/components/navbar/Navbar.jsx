78d4cd8bf36db494ab4ea9b9cdf966ca0d71122b
Semanur Özkan <semanurozkan4444@gmail.com>
2025-05-16 16:23:13 +0300

    refactor(navbar): integrate AuthContext and improve UI structure

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,15 +2,49 @@ import styles from "./navbar.module.scss";
 import { FaHeart } from "react-icons/fa";
 import { BsThreeDots } from "react-icons/bs";
 import { FaUserCircle } from "react-icons/fa";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MdOutlineAccountBox } from "react-icons/md";
 import { IoIosLogOut } from "react-icons/io";
+import { IoNotifications } from "react-icons/io5";
+import { TiMessages } from "react-icons/ti";
+import { IoMdSettings } from "react-icons/io";
+
+import axios from "axios";
+import { useAuth } from "../../context/AuthContext";
 
 const Navbar = () => {
+  const FETCH = import.meta.env.VITE_FETCH_URL;
+
+  const { user, logout } = useAuth();
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const modalRef = useRef(null);
   const buttonRef = useRef(null);
-  const [user, setUser] = useState(true);
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(`${FETCH}auth/logout`);
+      document.cookie = "token=; Max-Age=0";
+      logout();
+      setIsModalOpen(false);
+      window.location.href = "/login";
+    } catch (error) {
+      console.error("Çıkış yapılırken hata oluştu:", error);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setIsModalOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className={styles.navbar}>
@@ -46,18 +80,26 @@ const Navbar = () => {
               </button>
             </div>
           ) : (
-            <div
-              className={styles.user}
-              onClick={() => {
-                setIsModalOpen(!isModalOpen);
-              }}
-            >
-              <li>
-                <BsThreeDots className={styles.dots} />
-              </li>
-              <li>
-                <FaUserCircle className={styles.circle} />
-              </li>
+            <div className={styles.navEndContent}>
+              <a href="/messages">
+                <TiMessages className={styles.messages} />
+              </a>
+              <a href="/notification">
+                <IoNotifications className={styles.notification} />
+              </a>
+              <div
+                className={styles.user}
+                onClick={() => {
+                  setIsModalOpen(!isModalOpen);
+                }}
+              >
+                <li>
+                  <BsThreeDots className={styles.dots} />
+                </li>
+                <li>
+                  <FaUserCircle className={styles.circle} />
+                </li>
+              </div>
             </div>
           )}
 
@@ -67,10 +109,16 @@ const Navbar = () => {
                 <MdOutlineAccountBox className={styles.accountIcon} />
                 <a href="/profile">Hesabım</a>
               </div>
+
+              <div className={styles.modalItem} ref={buttonRef}>
+                <IoMdSettings className={styles.settingsIcon} />
+                <a href="/settings">Ayarlar</a>
+              </div>
+
               <div
                 className={styles.modalItem}
                 ref={buttonRef}
-                // onClick={handleLogout}
+                onClick={handleLogout}
               >
                 <IoIosLogOut className={styles.logoutIcon} />
                 Çıkış Yap
