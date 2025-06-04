fe0094a41725e35117cc19030dd4b7e538cee27d
Semanur Özkan <semanurozkan4444@gmail.com>
2025-06-04 10:31:16 +0300

    feat(addSummary): connect AddSummary form to backend API

--- src/pages/addSummary/AddSummary.jsx
+++ src/pages/addSummary/AddSummary.jsx
@@ -1,31 +1,45 @@
 import styles from "./addSummary.module.scss";
 import { useNavigate, useParams } from "react-router-dom";
 import { useState } from "react";
+import { useAuth } from "../../context/AuthContext";
+import axios from "axios";
 
 const AddSummary = () => {
+  const FETCH = import.meta.env.VITE_FETCH_URL;
+
   const { bookId } = useParams();
   const navigate = useNavigate();
+  const { user } = useAuth();
 
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [content, setContent] = useState("");
 
-  const handleSubmit = (e) => {
+  console.log("özet user:", user.user);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const newSummary = {
-      id: Date.now().toString(), // örnek ID
-      bookId,
       title,
-      author,
-      date: new Date().toLocaleDateString("tr-TR"),
+      author: user.user.fullname,
+      date: new Date().toLocaleDateString("tr-TR"), // "21.05.2025"
       content,
     };
 
-    console.log("Yeni Özet:", newSummary);
-    // TODO: backend'e gönderilecek (axios.post vs.)
+    try {
+      const res = await axios.post(
+        `${FETCH}books/${bookId}/summaries`,
+        newSummary,
+        { withCredentials: true }
+      );
 
-    navigate(`/book/${bookId}`);
+      if (res.status === 200) {
+        navigate(`/book/${bookId}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
